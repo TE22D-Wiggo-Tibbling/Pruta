13daa1d85620bfd29980d1106abe3a027bb78d27
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-11-07 10:56:09 +0100

    first commit

---
+++ Pruta/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Dynamic;
+using Raylib_cs;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+
+Raylib.InitWindow(800, 600, "wassa");
+
+while (!Raylib.WindowShouldClose()){
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLUE);
+    Raylib.EndDrawing();
+}
