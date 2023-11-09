93a532960aeb045f8b613f5e8662d2922d83025f
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-11-09 10:32:37 +0100

    fgdhdgthdtththrhtr

--- Pruta/Program.cs
+++ Pruta/Program.cs
@@ -5,10 +5,42 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 
 
-Raylib.InitWindow(800, 600, "wassa");
+// Raylib.InitWindow(800, 600, "wassa");
 
-while (!Raylib.WindowShouldClose()){
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.BLUE);
-    Raylib.EndDrawing();
+//------------------------------------------------------------------------------------------------
+//PRUT KOD
+//------------------------------------------------------------------------------------------------
+bool end = false;
+Random tärning = new Random();
+int nummer = tärning.Next(0,20);
+while(!end){
+    nummer = tärning.Next(0,20);
+    System.Console.WriteLine(nummer);
+   string svar= Console.ReadLine();
+   if(svar == "a"){
+    end = true;
+   }
 }
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------
+//GAME LOGIC
+//------------------------------------------------------------------------------------------------
+
+
+Console.ReadLine();
+
+//---------------------------------------------------------------------------------------------------
+//RENDER
+//---------------------------------------------------------------------------------------------------
+
+// while (!Raylib.WindowShouldClose()){
+//     Raylib.BeginDrawing();
+//     Raylib.ClearBackground(Color.BLUE);
+//     Raylib.EndDrawing();
+// }
