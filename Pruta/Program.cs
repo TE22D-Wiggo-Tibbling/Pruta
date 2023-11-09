a6964bb4d15ed542e99dfc396b91229d5cad8d77
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-11-09 10:38:34 +0100

    by8byubyuuybh

--- Pruta/Program.cs
+++ Pruta/Program.cs
@@ -3,20 +3,30 @@ using Raylib_cs;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
+//------------------------------------------------------------------------------------------------
+//ints och allt där imällan
+//------------------------------------------------------------------------------------------------
+int prutchans = 10;
+bool end = false;
+Random tärning = new Random();
+int nummer = tärning.Next(0,20);
 
-
-// Raylib.InitWindow(800, 600, "wassa");
 
 //------------------------------------------------------------------------------------------------
 //PRUT KOD
 //------------------------------------------------------------------------------------------------
-bool end = false;
-Random tärning = new Random();
-int nummer = tärning.Next(0,20);
+
 while(!end){
     nummer = tärning.Next(0,20);
     System.Console.WriteLine(nummer);
    string svar= Console.ReadLine();
+  
+  if(nummer > prutchans){
+    System.Console.WriteLine("ja");
+  }
+  
+  
+  
    if(svar == "a"){
     end = true;
    }
@@ -26,6 +36,8 @@ while(!end){
 
 
 
+Console.ReadLine();
+
 
 
 //------------------------------------------------------------------------------------------------
@@ -33,11 +45,13 @@ while(!end){
 //------------------------------------------------------------------------------------------------
 
 
-Console.ReadLine();
+
 
 //---------------------------------------------------------------------------------------------------
 //RENDER
 //---------------------------------------------------------------------------------------------------
+// Raylib.InitWindow(800, 600, "wassa");
+
 
 // while (!Raylib.WindowShouldClose()){
 //     Raylib.BeginDrawing();
